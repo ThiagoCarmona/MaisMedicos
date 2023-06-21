ec28c12b5018ee194ba141eefda7e00e7adb9463
Thiago Carmona <Thggcarmona@gmail.com>
2023-06-20 21:42:29 -0300

    Correção Botão

--- src/pages/concluido.tsx
+++ src/pages/concluido.tsx
@@ -8,24 +8,6 @@ export const Concluido = () => {
       }}>Concluido</h1>
       <p>Suas respostas foram enviadas, caso queira reenviar este questionário, por favor entre em contato com o administrador.</p>
       <p>andre.luiz@saude.gov.br</p>
-      <button
-        style={{
-          margin: 'auto',
-          display: 'block',
-          marginTop: '50px',
-          padding: '10px',
-          borderRadius: '5px',
-          border: 'none',
-          backgroundColor: '#ff1e1e',
-          color: '#fff',
-          cursor: 'pointer',
-          fontSize: '16px',
-          fontWeight: 'bold'
-        }}
-        onClick={() => window.close()}
-      >
-        Fechar
-      </button>
     </div>
   )
 }
